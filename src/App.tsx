dbeb0f3e8faf7226f979e10e1c7a81f8e3c04fb1
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-10 14:23:20 +0000

    ADD: Add bulma css

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import appLogo from "/favicon.svg";
-import PWABadge from "./PWABadge.tsx";
-import "./App.css";
+import React from "react";
+import "./styles/app.scss"
+import PWABadge from "./PWABadge";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="edumgmt-client-vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>edumgmt-client-vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+const App: React.FC<unknown> = () => (
+  <>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello World
+        </h1>
+        <p className="subtitle">
+          My first website with <strong>Bulma</strong>!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  );
-}
+      <div className="container">
+        <div className="buttons">
+          <button className="button is-primary">Primary</button>
+          <button className="button is-link">Link</button>
+        </div>
+
+        <div className="buttons">
+          <button className="button is-info">Info</button>
+          <button className="button is-success">Success</button>
+          <button className="button is-warning">Warning</button>
+          <button className="button is-danger">Danger</button>
+        </div>
+      </div>
+    </section>
+    <PWABadge />
+  </>
+)
 
 export default App;
