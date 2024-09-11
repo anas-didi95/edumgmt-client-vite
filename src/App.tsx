cf28cc2d6f1f1005c0fd88b725882fb825298bd0
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-11 13:51:58 +0000

    ADD: Add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import React from "react";
 import "./styles/app.scss";
 import PWABadge from "./PWABadge";
+import Navbar from "./components/Navbar";
 
 const App: React.FC<unknown> = () => (
   <>
-    <section className="section">
-      <div className="container">
-        <h1 className="title">Hello World</h1>
-        <p className="subtitle">
-          My first website with <strong>Bulma</strong>!
-        </p>
-      </div>
-      <div className="container">
-        <div className="buttons">
-          <button className="button is-primary">Primary</button>
-          <button className="button is-link">Link</button>
-        </div>
-
-        <div className="buttons">
-          <button className="button is-info">Info</button>
-          <button className="button is-success">Success</button>
-          <button className="button is-warning">Warning</button>
-          <button className="button is-danger">Danger</button>
-        </div>
-      </div>
-    </section>
+    <header>
+      <Navbar />
+    </header>
     <PWABadge />
   </>
 );
