38fcb75da655c68c2e97df29c0e5970f52165a47
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-11 14:10:25 +0000

    ADD: Add breadcrumb component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./styles/app.scss";
 import PWABadge from "./PWABadge";
 import Navbar from "./components/Navbar";
+import Breadcrumb from "./components/Breadcrumb";
 
 const App: React.FC<unknown> = () => (
   <>
     <header>
       <Navbar />
     </header>
+    <main>
+      <section className="section">
+        <div className="container">
+          <Breadcrumb />
+        </div>
+      </section>
+    </main>
     <PWABadge />
   </>
 );
---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Breadcrumb: React.FC<unknown> = () => (
+  <nav className="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="#">Bulma</a></li>
+      <li><a href="#">Documentation</a></li>
+      <li><a href="#">Components</a></li>
+      <li className="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+    </ul>
+  </nav>
+)
+
+export default Breadcrumb
