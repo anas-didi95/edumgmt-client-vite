880938bb945453e8eedb4792551c9cb5823e383a
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-12 14:54:15 +0000

    ADD: Add button group component

---
+++ src/components/ButtonGroup.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+interface IButtonGroup {
+  buttonList: {
+    label: string
+    type?: "success" | "light"
+  }[]
+}
+
+const ButtonGroup: React.FC<IButtonGroup> = ({ buttonList }) => (
+  <div className="buttons is-right">
+    {buttonList.map(({ label, type }, i) => (
+      <button key={`${label}${type}${i}`} className={`button ${!!type && "is-" + type}`}>{label}</button>
+    ))}
+  </div>
+)
+
+export default ButtonGroup
