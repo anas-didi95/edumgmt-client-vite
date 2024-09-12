488294b2061c35ebd990fbcbfb9ae1892eba6e23
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-12 14:54:33 +0000

    REF: Refactor component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import PWABadge from "./PWABadge";
 import Navbar from "./components/Navbar";
 import Breadcrumb from "./components/Breadcrumb";
 import Card from "./components/Card";
+import FormInput from "./components/FormInput";
+import ButtonGroup from "./components/ButtonGroup";
 
 const App: React.FC<unknown> = () => (
   <>
@@ -15,7 +17,24 @@ const App: React.FC<unknown> = () => (
         <div className="container">
           <Breadcrumb />
           <br />
-          <Card />
+          <Card title="Search User">
+            <div className="columns">
+              <div className="column is-4">
+                <FormInput label="User ID" type="text" />
+              </div>
+              <div className="column is-4">
+                <FormInput label="Name" type="text" />
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <ButtonGroup buttonList={[
+                  { label: "Reset" },
+                  { label: "Search", type: "success" }
+                ]} />
+              </div>
+            </div>
+          </Card>
         </div>
       </section>
     </main>
