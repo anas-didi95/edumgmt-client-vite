f91e7f7d69bb274b6c9f54471e990fb82775f0f5
Anas Juwaidi <anas.didi95@gmail.com>
2024-10-23 14:59:25 +0000

    ADD: Add form for user

--- src/routes/user/$userId.tsx
+++ src/routes/user/$userId.tsx
@@ -1,12 +1,45 @@
-import { FC } from "react";
+import { FC, useEffect, useMemo } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import UserService from "../../utils/services/UserService";
+import AppLayout from "../../layouts/AppLayout";
+import Card from "../../components/Card";
+import FormInput from "../../components/FormInput";
+import { UserFormType } from "../../utils/types/UserType";
+import { useForm } from "react-hook-form";
 
 const UserFormPage: FC<unknown> = () => {
   const { userId } = Route.useParams();
-  const { data } = UserService.useGetUser(userId);
+  const { data = {} as UserFormType } = UserService.useGetUser(userId);
+  const { register, reset } = useForm<UserFormType>({
+    defaultValues: useMemo(() => {
+      return data
+    }, [data]),
+  });
 
-  return <div>User Form Page: {JSON.stringify(data)}</div>;
+  useEffect(() => {
+    reset(data)
+  }, [reset, data]);
+
+  return (
+    <AppLayout breadcrumbList={["User", data.userId ?? "...", "View"]}>
+      <Card title="View User">
+        <form>
+          <div className="columns">
+            <div className="column is-4">
+              <FormInput
+                register={register("userId")}
+                label="User ID"
+                type="text"
+              />
+            </div>
+            <div className="column is-4">
+              <FormInput register={register("name")} label="Name" type="text" />
+            </div>
+          </div>
+        </form>
+      </Card>
+    </AppLayout>
+  );
 };
 
 export const Route = createFileRoute("/user/$userId")({
